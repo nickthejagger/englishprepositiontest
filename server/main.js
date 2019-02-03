bc1c0d0658c4c66d44def000dae050ff531e4a59
nichola indra <nichola.indra@gmail.com>
2019-02-03 16:15:25 +0700

    make it better

--- server/main.js
+++ server/main.js
@@ -1,8 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/api.js'
-function insertLink(title, url) {
-  Links.insert({ title, url, createdAt: new Date() });
-}
 
 Meteor.startup(() => {
 });
